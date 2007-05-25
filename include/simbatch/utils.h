42b76f1dd2970ca4203b662e95b15d1aee4297cf
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-05-25 14:42:03 +0000

    + add a header for each file to be lglpl compliant
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@4486 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/utils.h
+++ include/simbatch/utils.h
@@ -1,3 +1,14 @@
+/****************************************************************************/
+/* This file is part of the Simbatch project                                */
+/* written by Jean-Sebastien Gay, ENS Lyon                                  */
+/*                                                                          */
+/* Copyright (c) 2007 Jean-Sebastien Gay. All rights reserved.              */
+/*                                                                          */
+/* This program is free software; you can redistribute it and/or modify it  */
+/* under the terms of the license (GNU LGPL) which comes with this package. */
+/****************************************************************************/
+
+
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
