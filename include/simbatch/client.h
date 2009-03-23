6947903904ebb2eb1fd9b129c7abdd1d7b4835f8
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-03-23 18:08:49 +0000

    Document the file in doxygen style.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6169 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/client.h
+++ include/simbatch/client.h
@@ -1,8 +1,8 @@
 /****************************************************************************/
 /* This file is part of the Simbatch project                                */
-/* written by Jean-Sebastien Gay, ENS Lyon                                  */
+/* written by Jean-Sebastien Gay and Ghislain Charrier, ENS Lyon.           */
 /*                                                                          */
-/* Copyright (c) 2007 Jean-Sebastien Gay. All rights reserved.              */
+/* Copyright (c) 2007, Simbatch team. All rights reserved.                  */
 /*                                                                          */
 /* This program is free software; you can redistribute it and/or modify it  */
 /* under the terms of the license (GNU LGPL) which comes with this package. */
@@ -11,14 +11,20 @@
 
 
 /*
- * This file should be removes. It is used to simplify a client example
+ * This file should be removes. They were disgned for running tests.
+ * Those clients could move in the example directory.
  */
 
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
-int SB_client(int argc, char ** argv);
-int SB_file_client(int argc, char ** argv);
-int SB_job_client(int argc, char ** argv);
+int
+SB_client(int argc, char **argv);
+
+int
+SB_file_client(int argc, char **argv);
+
+int
+SB_job_client(int argc, char **argv);
 
 #endif
--- include/simbatch/cluster.h
+++ include/simbatch/cluster.h
@@ -1,8 +1,8 @@
 /****************************************************************************/
-/* This file is part of the Simbatch project                                */
-/* written by Jean-Sebastien Gay, ENS Lyon                                  */
+/* This file is part of the Simbatch project.                               */
+/* written by Jean-Sebastien Gay and Ghislain Charrier, ENS Lyon.           */
 /*                                                                          */
-/* Copyright (c) 2007 Jean-Sebastien Gay. All rights reserved.              */
+/* Copyright (c) 2007, Simbatch team. All rights reserved.                  */
 /*                                                                          */
 /* This program is free software; you can redistribute it and/or modify it  */
 /* under the terms of the license (GNU LGPL) which comes with this package. */
@@ -17,42 +17,89 @@
 #include <msg/msg.h>
 
 #include "job.h"
-/*
- * Cluster definition
- * - Number of slaves
- * - Number of priority's level
- * - The slaves
- * - Trace of the tasks actually in the system (batch + cluster)
- * - Scheduling table
- * - Reservation queue
+
+/**
+ * Cluster definition.
+ * This is a representation of the cluster for the SB_bacth process.
  */
 typedef struct {
+    
+    /** Number of slaves. */
     int nb_nodes;
+    
+    /** Number of priority level available for a task. 
+     *  Each level has its own queue. 
+     */
     int priority;
-    m_host_t * nodes;
-    xbt_dynar_t * queues;
-    xbt_dynar_t * waiting_queue;
+    
+    /** List of slaves. */
+    m_host_t *nodes;
+    
+    /** Trace of tasks actually in the systeme (batch + cluster). */
+    xbt_dynar_t *queues;
+    
+    /** Scheduling tabe or Gantt diagramm. */
+    xbt_dynar_t *waiting_queue;
+    
+    /** Reservation queue */ 
     xbt_dynar_t reservations;
-} * cluster_t;
+    
+} *cluster_t;
 
 
-/* Request a cluster with the params defined in the xml files */
-cluster_t  SB_request_cluster(int argc, char ** argv);
+/**
+ * Cluster constructor.
+ * The cluster representation is built with the parameters defined in the
+ * deployment.xml files.
+ * \param argc number of parameters.
+ * \param **argv list of parameters.
+ * \return a pointer on the cluster structure.
+ */
+cluster_t
+SB_request_cluster(int argc, char **argv);
 
-/* Free a cluster */ 
-void cluster_destroy(cluster_t  * cluster);     
+/**
+ * Free a cluster structure. 
+ * \param *cluster the cluster structure to be freed.
+ */ 
+void
+cluster_destroy(cluster_t  *cluster);     
 
-/* Remove only job that are in WAITING state (waiting) */
-void cluster_clean(cluster_t cluster);
+/**
+ * Remove jobs in WAITING state (waiting) from queues.
+ * \param cluster the cluster containing the queues. 
+ */
+void
+cluster_clean(cluster_t cluster);
 
-/* Search a job by its id */
-int cluster_search_job(cluster_t cluster, int job_id, xbt_dynar_t dynar);
+/**
+ * Search a job by its id.
+ * \param cluster deprecated parameter 
+ * \param job_id identifiant of the searched job.
+ * \param dynar structure containing the job.
+ * \return -1 if the search failed, index of the job_id in dynar otherwise.
+ */
+int 
+cluster_search_job(cluster_t cluster, int job_id, xbt_dynar_t dynar);
 
-/* Delete a job in the cluster (job should be at first place (running)*/
-void cluster_delete_done_job(cluster_t cluster, job_t job);
+/**
+ * Delete a job in the cluster.
+ * This function is called when the job becomes done. All the SB_ACK have benn
+ * been received. So The job should be at the first place in the scheduling
+ * table as it is in a running state.
+ * \param cluster the cluster repreentation.
+ * \param job the job to delete from the scheduling table.
+ */
+void 
+cluster_delete_done_job(cluster_t cluster, job_t job);
 
-/* Prints some infos on a cluster */
-void cluster_print(cluster_t cluster);
+/** 
+ * Prints some infos on a cluster.
+ * Used as a debug function.
+ * \param the cluster representation. 
+ */
+void 
+cluster_print(cluster_t cluster);
 
 /* return the number of priority queue for the cluster 
  * Unused Function in the project
