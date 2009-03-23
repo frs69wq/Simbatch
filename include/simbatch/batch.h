fcead07c6f0a7f919a2856257a37f9efd453c524
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-03-23 18:06:14 +0000

    Add documentation in doxygen style (use JAVA_BRIEF)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6168 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/batch.h
+++ include/simbatch/batch.h
@@ -1,8 +1,8 @@
 /****************************************************************************/
-/* This file is part of the Simbatch project                                */
-/* written by Jean-Sebastien Gay, ENS Lyon                                  */
+/* This file is part of the Simbatch project.                               */
+/* written by Jean-Sebastien Gay and Ghislain Charrier, ENS Lyon.           */
 /*                                                                          */
-/* Copyright (c) 2007 Jean-Sebastien Gay. All rights reserved.              */
+/* Copyright (c) 2007, Simbatch Team. All rights reserved.                  */
 /*                                                                          */
 /* This program is free software; you can redistribute it and/or modify it  */
 /* under the terms of the license (GNU LGPL) which comes with this package. */
@@ -12,18 +12,34 @@
 #ifndef _BATCH_H_
 #define _BATCH_H_
 
-/*
- * Simulates the behavior of a Batch system
- * MSG_tasks to use when calling the function:
- * SB_TASK
- * SB_RES to make reservations
- * SB_ACK when a task has been done
- * SB_DIET when working with DIET
- * SED_PRED to perform a prediction of when the task will 
- *   be able to execute
- * SED_HPF 
- * PF_INIT to initialize the batch
+/**
+ * \file batch.h
+ * Define the batch process.
  */
-int SB_batch(int argc, char ** argv);
+
+
+/**
+ * Simulates the behavior of a Batch system.
+ *
+ * The behaviour of the batch process consists in responding to incoming 
+ * messages and to schedule jobs sent by clients. Messages are MSG_task 
+ * datatype provided by the simgrid library.
+ *
+ * Here is a short description of the different tasks received:
+ * SB_TASK conatins the job to schedule.
+ * SB_RES to make reservations.
+ * SB_ACK when a task has been done on a cpu. 5 cpus for a task => 5 SB_ACK.
+ * SB_DIET to allow Diet for using Simbatch.
+ * SED_PRED to perform a prediction of when the task will be able to execute.
+ * SED_HPF (work in progess)
+ * PF_INIT to initialize the batch
+ *
+ * \param argc number of parameters transmitted to the SB_batch process.
+ * \param **argv array containing the parameters. argc and argc are 
+ * automacilly filled by simgrid when parsing the deployment.xml file.
+ * \return an error code.
+ */
+int 
+SB_batch(int argc, char **argv);
 
 #endif
