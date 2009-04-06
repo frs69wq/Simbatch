ed92d4deb4f122fa46f1883874f1237c90e5bf2b
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:34:03 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6206 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/plugin.h
+++ include/simbatch/plugin.h
@@ -17,25 +17,29 @@
  * We use only plugin scheduler
  ***********************/
 
-typedef struct _plugin_t 
-{
-  void * content;
-  void * handle;
-  const char * name;
-  const char * init_fct;
-} pluginInfo, * pluginInfo_t;
+typedef struct pluginInfo {
+  void *content;
+  void *handle;
+  const char *name;
+  const char *init_fct;
+} pluginInfo, *pluginInfo_t;
 
 
 /* Return an error code (0 => OK) */
-int launch_plugin(void ** handle, void * plugin, 
-		  const char * plugin_name, const char * init_fct);
+int
+launch_plugin(void **handle, void *plugin, const char *plugin_name,
+              const char *init_fct);
 
-int launch_plugin2(pluginInfo_t plugin);
+int
+launch_plugin2(pluginInfo_t plugin);
 
-__inline__ void close_plugin(void * handle);
+__inline__ void
+close_plugin(void *handle);
 
-__inline__ void close_pluginInfo(void * data);
+__inline__ void
+close_pluginInfo(void *data);
 
-pluginInfo_t  SB_request_plugin(const char * init_function, const size_t size);
+pluginInfo_t
+SB_request_plugin(const char *init_function, const size_t size);
 
 #endif
