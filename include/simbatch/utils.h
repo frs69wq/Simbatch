f2cd347c442d3d49be8af505a8a11c0b4df896a4
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:34:05 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6207 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/utils.h
+++ include/simbatch/utils.h
@@ -24,7 +24,10 @@
 /* Return the name of the process calling the macro */
 #define PROCESS_NAME() MSG_process_get_name(MSG_process_self())
 
-void xbt_fifo_sort(xbt_fifo_t fifo);
-m_process_t MSG_task_async_put(m_task_t task, m_host_t host, m_channel_t channel);
+void
+xbt_fifo_sort(xbt_fifo_t fifo);
+
+m_process_t
+MSG_task_async_put(m_task_t task, m_host_t host, m_channel_t channel);
 
 #endif
