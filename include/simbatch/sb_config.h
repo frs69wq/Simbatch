06a4e9bce6d3bc5e125033520864e51c9be2e365
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:33:58 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6204 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/sb_config.h
+++ include/simbatch/sb_config.h
@@ -19,18 +19,23 @@
  */
 
 /* Call to simbatch init */
-__inline__ void SB_global_init(int * argc, char ** argv);
+__inline__ void
+SB_global_init(int *argc, char **argv);
 
 /* Call to simbatch clean */
-__inline__ void SB_clean(void);
+__inline__ void
+SB_clean(void);
 
 /* Get the name of the platform file from the config */
-__inline__ const char * SB_get_platform_file(void);
+__inline__ const char *
+SB_get_platform_file(void);
 
 /* Get the name of the platform file from the config */
-__inline__ const char * SB_get_deployment_file(void);
+__inline__ const char *
+SB_get_deployment_file(void);
 
 /* Get the name of the trace file from the config */
-__inline__ const char * SB_get_trace_file(void);
+__inline__ const char *
+SB_get_trace_file(void);
 
 #endif
