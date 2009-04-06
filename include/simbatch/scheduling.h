ba153f317e52a7a8693c548f23f7823e97cf8e1d
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:34:15 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6212 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/scheduling.h
+++ include/simbatch/scheduling.h
@@ -19,49 +19,63 @@
 
 /****************** Generic rescheduling functions ********************/
 /* No re-schedule when a task just comes to end */
-// void generic_no_reschedule(cluster_t cluster, scheduling_fct schedule);
-void generic_no_reschedule(cluster_t cluster, plugin_scheduler_t scheduler);
+// void generic_no_reschedule(m_cluster_t cluster, scheduling_fct schedule);
+void
+generic_no_reschedule(m_cluster_t cluster, plugin_scheduler_t scheduler);
 
 /* Function to reschedule jobs waiting in the system */
-// void generic_reschedule (cluster_t cluster, scheduling_fct schedule);
-void generic_reschedule(cluster_t cluster, plugin_scheduler * scheduler);
+// void generic_reschedule (m_cluster_t cluster, scheduling_fct schedule);
+void
+generic_reschedule(m_cluster_t cluster, plugin_scheduler *scheduler);
 
 /********************* Slot management functions *********************/
-slot_t new_slot(int node_id, int position, double start_time, double duration);
+slot_t
+new_slot(int node_id, int position, double start_time, double duration);
 
-int comp(const void * slot1, const void * slot2);
+int
+comp(const void *slot1, const void *slot2);
 
-int check_matching_slot(slot_t * slots, int nb_nodes);
+int
+check_matching_slot(slot_t *slots, int nb_nodes);
 
-slot_t search_a_slot(cluster_t cluster, int node_id, int it,
+slot_t search_a_slot(m_cluster_t cluster, int node_id, int it,
 		     double start_time, double end_time, double duration);
 
-slot_t * find_a_slot(cluster_t cluster, int nb_nodes,
-		     double start_time, double duration);
+slot_t *
+find_a_slot(m_cluster_t cluster, int nb_nodes, double start_time,
+            double duration);
 
 
 /* Return the next job to schedule in the queue*/
-job_t next_job_to_schedule_in_queue(xbt_dynar_t queue);
+job_t
+next_job_to_schedule_in_queue(xbt_dynar_t queue);
 
 /* Return the next job to schedule */
-job_t next_job_to_schedule(cluster_t cluster);
+job_t
+next_job_to_schedule(m_cluster_t cluster);
 
 
 /* usefull to build xbt_dynar of bids (xbt dynar request) */
-void free_slot(void * d);
+void
+free_slot(void *d);
 
 /* print a xbt dynar of slots */
-__inline__ void print_slots(xbt_dynar_t slots);
+__inline__ void
+print_slots(xbt_dynar_t slots);
 
 /* return the last slot of queue i */
-slot_t get_last_slot(cluster_t cluster, int i);
+slot_t
+get_last_slot(m_cluster_t cluster, int i);
 
 /* return the n best slots sorted by start time (no check for duration) */
-slot_t * select_n_slots(cluster_t cluster, xbt_dynar_t slots, int nb);
+slot_t *
+select_n_slots(m_cluster_t cluster, xbt_dynar_t slots, int nb);
 
 /* return the last completion time (end of work) */
-double get_completion_time(cluster_t cluster);
+double
+get_completion_time(m_cluster_t cluster);
 
-__inline__ void print_slot(slot_t * slots, int size);
+__inline__ void
+print_slot(slot_t *slots, int size);
 
 #endif
