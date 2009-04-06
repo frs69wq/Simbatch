03754079336bd552e20e8f2315bd394bf83190af
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:34:09 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6209 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/simbatch_config.h
+++ include/simbatch/simbatch_config.h
@@ -25,31 +25,40 @@ typedef struct config_t {
 
 
 /* Init simbatch library */
-void simbatch_init(int * argc, char ** argv);
+void
+simbatch_init(int *argc, char **argv);
 
 /* Close handlers and other stuff */
-void simbatch_clean(void);
+void
+simbatch_clean(void);
 
 /* Load an xml file - DOM tree */
-config_t * config_load(const char * filename);
+config_t *
+config_load(const char *filename);
 
 /* Free the global config */
-void config_close(void);
+void
+config_close(void);
 
 /* Get the whole answer structure */
-xmlXPathObjectPtr config_get(const char * _xpath);
+xmlXPathObjectPtr
+config_get(const char *_xpath);
 
 /* Get the first value from an XPath request */
-const char * config_get_value(const char * _xpath);
+const char *
+config_get_value(const char *_xpath);
 
 /* Get number of nodes matching an XPath request */
-int config_get_nb_nodes(const char * _path);
+int
+config_get_nb_nodes(const char *_path);
 
 /* Get the name of the platform file from the config */
-__inline__ const char * config_get_platform_file(void);
+__inline__ const char *
+config_get_platform_file(void);
 
 /* Get the name of the deployment file from the config */
-__inline__ const char * config_get_deployment_file(void);
+__inline__ const char *
+config_get_deployment_file(void);
 
 /* Get the name of the trace file from the config */
 __inline__ const char * config_get_trace_file(void);
@@ -58,12 +67,15 @@ __inline__ const char * config_get_trace_file(void);
 void config_init_log_file(void);
 
 /* Get the descriptor for the logfile associated to an host */
-FILE * config_get_log_file(const char * hostname);
+FILE *
+config_get_log_file(const char *hostname);
 
 /* Return a plugin from the book of plugin - NULL otherwise*/
-__inline__ void * config_get_plugin(const char * plugin_name);
+__inline__ void *
+config_get_plugin(const char *plugin_name);
 
 /* Set a new plugin in the book of plugin */
-__inline__ void config_set_plugin(const char * plugin_name, void * plugin, void * close_plugin);
+__inline__ void
+config_set_plugin(const char *plugin_name, void *plugin, void *close_plugin);
 
 #endif
