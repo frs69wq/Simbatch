9dce5bbb2d52155c4e7326bcc83deee4769068e7
jsgay <jsgay@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-04-06 18:36:59 +0000

    1. Replace the cluster_t type:     . cluster_t is now the base type     . m_cluster_t is a pointer on cluster_t (typedef cluster_t *m_cluster_t)
    2. Re-Indent parts of code
    
    3. Modify Makefile in Batch example
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6215 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/plugins/scheduler/rrobin.c
+++ src/plugins/scheduler/rrobin.c
@@ -24,10 +24,13 @@
 ///// DEPRECATED
 
 /************* Functions *************/
-static slot_t * rrobin_schedule(cluster_t cluster, job_t job);
+static slot_t *
+rrobin_schedule(m_cluster_t cluster, job_t job);
 
 /**************** Code ***************/
-plugin_scheduler_t init (plugin_scheduler_t p) {
+plugin_scheduler_t
+init (plugin_scheduler_t p)
+{
     p->schedule = rrobin_schedule;
     p->reschedule = generic_reschedule;
     
@@ -35,35 +38,39 @@ plugin_scheduler_t init (plugin_scheduler_t p) {
 }
 
 
-static slot_t * rrobin_schedule(cluster_t cluster, job_t job) {
+static slot_t *
+rrobin_schedule(m_cluster_t cluster, job_t job)
+{
     static int proc = 0;
     int j = 0, k = proc;
     job->start_time = 0.0;
     
     /* We take the bid for each queue */
     for (j = 0; j < job->nb_procs; ++j) {
-	slot_t b = NULL;
+        slot_t b = NULL;
 	
-	/* a speedup could be to not return a bid (no reusability) */
-	b = get_last_slot(cluster, k);
-	if (b->start_time > job->start_time) {
-	    job->start_time = b->start_time;
-	    job->completion_time = b->start_time + job->wall_time;
-	}
-	k = (k+1) % cluster->nb_nodes;
-	xbt_free(b);
+        /* a speedup could be to not return a bid (no reusability) */
+        b = get_last_slot(cluster, k);
+        if (b->start_time > job->start_time) {
+            job->start_time = b->start_time;
+            job->completion_time = b->start_time + job->wall_time;
+        }
+        k = (k+1) % cluster->nb_nodes;
+        xbt_free(b);
     }
     
     for (j=0; j<job->nb_procs; ++j) {
-	job->mapping[j] = proc;
-	xbt_dynar_push(cluster->waiting_queue[proc], &job);
-	proc = (proc+1) % cluster->nb_nodes;
+        job->mapping[j] = proc;
+        xbt_dynar_push(cluster->waiting_queue[proc], &job);
+        proc = (proc+1) % cluster->nb_nodes;
     }
 
     return NULL;
 }
 
-static void rrobin_accept(cluster_t cluster, job_t job, slot_t * slots) {
+static void
+rrobin_accept(m_cluster_t cluster, job_t job, slot_t *slots)
+{
     return;
 }
 
