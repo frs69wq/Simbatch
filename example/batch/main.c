5d20e18fce28bc8d2bcab1b0b0256495d66319c6
gcharrie <gcharrie@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-09 15:11:15 +0000

    - update batch example by removing the channels  - removing trace file  - adding a new load (old ones are not working anymore)  - removing all references to the client part because it did nothing
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6871 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- example/batch/main.c
+++ example/batch/main.c
@@ -16,25 +16,10 @@
 #include "simbatch.h"
 #include "simbatch/sb_config.h"
 
-#define NB_CHANNEL 10000
-
 int main(int argc, char ** argv) {
-    const char * trace_file = NULL;
-    
     SB_global_init(&argc, argv);
     MSG_global_init(&argc, argv);
- 
-    /* Open the channels */
-    MSG_set_channel_number(NB_CHANNEL);
     
-    /* Need a trace */
-    trace_file = SB_get_trace_file();
-    if (trace_file != NULL)
-	MSG_paje_output(trace_file);
-    
-    /* The client that submits requests (write your own ) 
-     * params have to be called with the same name */
-    MSG_function_register("SB_client", SB_client);
     /* The batch */
     MSG_function_register("SB_batch", SB_batch);
     /* Node of the Cluster */
