72ce552c20c7933cc6f2ab89f7dd76e8682ca8b6
gcharrie <gcharrie@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-09 14:56:33 +0000

    - adding deadline attribute to jobs  - indentation  - changing some attributes types (int -> unsigned int)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/Simbatch@6868 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/simbatch/job.h
+++ include/simbatch/job.h
@@ -27,13 +27,14 @@
  * States a job can have.
  */
 typedef enum state_t {
-    WAITING    = 0,
-    LOADING    = 1,
-    PROCESSING = 2,
-    RECOVERING = 3,
-    DONE       = 4,
-    CANCELLED  = 5,
-    RESERVED   = 6
+  WAITING    = 0,
+  LOADING    = 1,
+  PROCESSING = 2,
+  RECOVERING = 3,
+  DONE       = 4,
+  CANCELLED  = 5,
+  RESERVED   = 6,
+  ERROR      = 7
 } state_t;
 
 
@@ -43,26 +44,27 @@ typedef enum state_t {
  * types.
  */
 typedef struct _job {
-    char name[15];      /*!< Name of the job. */
-    double submit_time; /*!< Time client send the job. */
-    double entry_time;  /*!< First time the job enters the batch. */
-    double run_time;    /*!< Time needed to run. */
-    double input_size;  /*!< Size to be transferred before execution. */
-    double output_size; /*!< Size of produced data. */
-    double wall_time;   /*!< Duration for a slot requested by the job. */
-    double start_time;  /*!< Time when job starts its execution. */
-    double completion_time;     /*!< Time when job completes its execution. */
-    double weight;              /*!< A weight given by some heuristic. */
-    unsigned long int user_id;  /*!< Jobs'id given by the user. */
-    unsigned long int id;   /*!< Jobs'id given by the batch */
-    int priority;       /*<! priority of a job. */
-    int nb_procs;       /*<! Number of procs needed. */
-    unsigned int service;   /*<! Service that can execute the job. */
-    int * mapping;      /*<! Proc affected for the job. */
-    state_t state;      /*<! Current state of a job. */
-    m_host_t source;    /*<! The m_host_t which send the task. */
-    int free_on_completion; /*<! Job to free when completed */
-    void * data;        /*<! some datas */
+  char name[15];      /*!< Name of the job. */
+  double submit_time; /*!< Time client send the job. */
+  double entry_time;  /*!< First time the job enters the batch. */
+  double run_time;    /*!< Time needed to run. */
+  double input_size;  /*!< Size to be transferred before execution. */
+  double output_size; /*!< Size of produced data. */
+  double wall_time;   /*!< Duration for a slot requested by the job. */
+  double start_time;  /*!< Time when job starts its execution. */
+  double completion_time;     /*!< Time when job completes its execution. */
+  double weight;              /*!< A weight given by some heuristic. */
+  double deadline;    /*!< Deadline to complete the job */
+  unsigned long int user_id;  /*!< Jobs'id given by the user. */
+  unsigned long int id;   /*!< Jobs'id given by the batch */
+  int priority;       /*<! priority of a job. */
+  unsigned int nb_procs;       /*<! Number of procs needed. */
+  unsigned int service;   /*<! Service that can execute the job. */
+  int * mapping;      /*<! Proc affected for the job. */
+  state_t state;      /*<! Current state of a job. */
+  m_host_t source;    /*<! The m_host_t which send the task. */
+  int free_on_completion; /*<! Job to free when completed */
+  void * data;        /*<! some datas */
 } * job_t;
 
 
@@ -73,12 +75,12 @@ typedef struct _job {
  * Only full slots are technically existing in Simbatch.
  */
 typedef struct _slot {
-    int node;           /*<! Number designing the job owning the slot. */
-    int position;       /*<! Position in the waiting queue. */
-    double start_time;  /*<! Start time of the slot.*/
-    double duration;    /*<! Slot duration. */
-    m_host_t host;      /*<! Usefull for a metascheduler. */
-    void * data;        /*<! Could be usefull. */
+  unsigned int node;     /*<! Number designing the job owning the slot. */
+  unsigned int position; /*<! Position in the waiting queue. */
+  double start_time;     /*<! Start time of the slot.*/
+  double duration;       /*<! Slot duration. */
+  m_host_t host;         /*<! Usefull for a metascheduler. */
+  void * data;           /*<! Could be usefull. */
 } slot, * slot_t;
 
 
--- include/simbatch/utils.h
+++ include/simbatch/utils.h
@@ -15,9 +15,6 @@
 #include <xbt/fifo.h>
 #include <msg/msg.h>
 
-/* Return the tab's size */
-#define TAB_SIZE(t) sizeof(t)/sizeof(*t)
-
 /* Return the name of the host calling the macro */
 #define HOST_NAME() MSG_host_get_name(MSG_host_self())
 
@@ -25,10 +22,7 @@
 #define PROCESS_NAME() MSG_process_get_name(MSG_process_self())
 
 void
-xbt_fifo_sort(xbt_fifo_t fifo);
-
-m_process_t
-MSG_task_async_put(m_task_t task, m_host_t host, m_channel_t channel);
+xbt_fifo_alphabetically_sort(xbt_fifo_t fifo);
 
 m_process_t
 MSG_task_async_send(m_task_t task, char * mailbox);
